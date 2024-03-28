a6754e3070952f393a2b05b3f78779e850859aa6
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2024-03-28 19:59:50 +0900

    Add files via upload

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @GetMapping("/")
     public String home(){
+        log.info("check");
         return "Hello";
     }
 }
